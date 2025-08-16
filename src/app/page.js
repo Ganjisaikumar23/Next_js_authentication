d5b3ee6a89e7714b54fb5013b61bec23eb19aadd
Ganji Sai Kumar <ganjisaikumar23@gmail.com>
2025-08-16 11:32:16 +0530

    authentication completed with github,google

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { auth } from "./auth";
+import { redirect } from "next/navigation";
+import Link from "next/link";
 
-const LandingPage = () => {
+const LandingPage = async () => {
+  const session = await auth();
+  if (!session) {
+    redirect("api/auth/signin");
+  }
+
   return (
-    <div> LandingPage</div>
-  )
-}
+    <>
+      <div> LandingPage</div>
+      <Link href="/api/auth/signout">
+        <button>LogOut </button>
+      </Link>
+    </>
+  );
+};
 
-export default LandingPage
+export default LandingPage;
